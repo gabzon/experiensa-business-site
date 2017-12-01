72935fafc75754a3a3a829d707a1b94c271ba44c
gabzon <gab.zambrano@gmail.com>
2017-12-01 17:53:22 +0100

    piklist

---
+++ resources/piklist/parts/meta-boxes/media.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Title: Media
+Post Type: attachment
+*/
+
+piklist('field', [
+  'type'    => 'text',
+  'field'   => 'exp_media_url',
+  'label'   => 'Media URL',
+  'columns' => 12,
+]);
