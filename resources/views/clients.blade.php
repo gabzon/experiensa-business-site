4b95454e80aeb41063e9fae4570d6c4aead30c42
gabzon <gab.zambrano@gmail.com>
2018-04-24 00:37:08 +0200

    padding bottom + card-columns

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -30,7 +30,7 @@ Template Name: Clients
       @while ( $operator->have_posts() )
         @php($operator->the_post())
           <a href="{{ get_post_meta(get_the_ID(), 'exp_client_website', true) }}">
-            <div class="card mb3 pt1">
+            <div class="card mb3 pv2">
               <img class="card-img-top" src="{{ get_the_post_thumbnail_url( get_the_ID(),'post-thumbnail' ) }}" alt="{{ get_the_title() }}">
               {{-- <div class="card-body"> --}}
                 {{-- <h5 class="card-title tc">{{ get_the_title() }}</h5> --}}
@@ -68,7 +68,7 @@ Template Name: Clients
       @while ( $agency->have_posts() )
         @php($agency->the_post())
           <a href="{{ get_post_meta(get_the_ID(), 'exp_client_website', true) }}" target="_blank">
-            <div class="card mb3 pt1">
+            <div class="card mb3 pv2">
               <img class="card-img-top" src="{{ get_the_post_thumbnail_url( get_the_ID(),'post-thumbnail' ) }}" alt="Card image cap">
               <div class="card-body">
                 {{-- <h5 class="card-title tc">{{ get_the_title() }}</h5> --}}
