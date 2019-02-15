32387080bc4d90c35291f8ba262cf35b2c7a3619
gabzon <gab.zambrano@gmail.com>
2019-02-15 01:54:52 +0100

    media custom fields added

---
+++ app/api/media.php
@@ -0,0 +1,56 @@
+<?php
+
+add_action( 'rest_api_init', 'add_custom_fields' );
+
+function add_custom_fields() {
+
+  register_rest_field('attachment','categories',  ['get_callback' => 'get_media_categories_field']);
+  register_rest_field('attachment','places',      ['get_callback' => 'get_media_places_field']);
+  register_rest_field('attachment','countries',   ['get_callback' => 'get_media_countries_field']);
+  register_rest_field('attachment','season',      ['get_callback' => 'get_media_season_field']);
+  register_rest_field('attachment','authors',     ['get_callback' => 'get_media_author_field']);
+  register_rest_field('attachment','daylight',    ['get_callback' => 'get_media_daylight_field']);
+  register_rest_field('attachment','destinations',['get_callback' => 'get_media_destinations_field']);
+  register_rest_field('attachment','format',      ['get_callback' => 'get_media_format_field']);
+
+}
+
+// categories
+function get_media_categories_field($object, $field_name, $request){
+  return wp_get_post_terms( $object['id'], 'media_category', array("fields" => "names"));
+}
+
+// places
+function get_media_places_field($object, $field_name, $request){
+  return wp_get_post_terms( $object['id'], 'media_place', array("fields" => "names"));
+}
+
+// countries
+function get_media_countries_field($object, $field_name, $request){
+  return wp_get_post_terms( $object['id'], 'media_country', array("fields" => "names"));
+}
+
+// season
+function get_media_season_field($object, $field_name, $request){
+  return wp_get_post_terms( $object['id'], 'media_season', array("fields" => "names"));
+}
+
+// author
+function get_media_author_field($object, $field_name, $request){
+  return wp_get_post_terms( $object['id'], 'media_author', array("fields" => "names"));
+}
+
+// daylight
+function get_media_daylight_field($object, $field_name, $request){
+  return wp_get_post_terms( $object['id'], 'media_daylight', array("fields" => "names"));
+}
+
+// destination
+function get_media_destinations_field($object, $field_name, $request){
+  return wp_get_post_terms( $object['id'], 'media_destination', array("fields" => "names"));
+}
+
+// Format
+function get_media_format_field($object, $field_name, $request){
+  return wp_get_post_terms( $object['id'], 'media_format', array("fields" => "names"));
+}
--- resources/functions.php
+++ resources/functions.php
@@ -58,7 +58,15 @@ array_map(function ($file) use ($sage_error) {
   if (!locate_template($file, true, true)) {
     $sage_error(sprintf(__('Error locating <code>%s</code> for inclusion.', 'sage'), $file), 'File not found');
   }
-}, ['helpers', 'setup', 'filters', 'admin', 'cpt/client', 'inc/wp-bootstrap-pagination']);
+}, [
+  'helpers',
+  'setup',
+  'filters',
+  'admin',
+  'cpt/client',
+  'api/media',
+  'inc/wp-bootstrap-pagination'
+]);
 
 /**
 * Here's what's happening with these hooks:
